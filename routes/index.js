df6402870400359e6323970e0178089d4583ae77
josepereza <48152618+josepereza@users.noreply.github.com>
2019-08-10 09:40:12 +0200

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+const mariadb = require('mariadb');
+var db = mariadb.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'seq_db'
+})
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/ver', function(req, res, next) {
+  console.log('hola');
+
+ res.render('lista1')
+});
+
+router.post('/datos', function(request, response){
+  
+  console.log(request.body.user.name);
+  console.log(request.body.user.email);
+  response.render('listapost',{title: request.body.user});
+});
+
+
+router.get('/listado', function(req, res, next) {
+  console.log('listado');
+  db.then(conn => {
+    conn.query("select * from Usuarios")
+        .then(rows => {
+            res.render('listado', {rows});
+
+        })
+})
+});
+
+router.get('/prueba', function(req, res, next) {
+  console.log('hola');
+
+ res.render('prueba')
+});
+module.exports = router;
